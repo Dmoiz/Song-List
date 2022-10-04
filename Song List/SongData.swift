d6c2b603282c18cfa4587835f6d1003d174e4480
Diego Moreno <95129545+Dmoiz@users.noreply.github.com>
2022-10-05 00:25:50 +0200

    init
    init and new classes

---
+++ Song List/SongData.swift
@@ -0,0 +1,13 @@
+//
+//  SongData.swift
+//  Song List
+//
+//  Created by Diego Moreno on 4/10/22.
+//
+
+import UIKit
+
+class songData : UITableViewCell {
+    
+}
+
---
+++ Song List/SongList.swift
@@ -0,0 +1,22 @@
+//
+//  SongList.swift
+//  Song List
+//
+//  Created by Diego Moreno on 4/10/22.
+//
+
+import UIKit
+
+class songList : UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
+}
+
