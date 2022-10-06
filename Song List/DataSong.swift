8a666eacecf657a6b7c546241fcd7286dea3d46a
Diego Moreno <95129545+Dmoiz@users.noreply.github.com>
2022-10-06 20:14:36 +0200

    Adding little things

---
+++ Song List/DataSong.swift
@@ -0,0 +1,20 @@
+//
+//  SongData.swift
+//  Song List
+//
+//  Created by Diego Moreno on 4/10/22.
+//
+
+import UIKit
+
+protocol dataSongs {
+    var imageUrl: String { get set}
+    var song: String { get set}
+    var author: String { get set}
+    
+    // Getters
+    func getImageUrl() -> String
+    func getTitle() -> String
+    func getSubTitle() -> String
+
+}
